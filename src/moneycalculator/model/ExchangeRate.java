8edfa26c4417485917fe38d0d7863be8502ee4e8
usuario <usuario@10.22.143.16>
2014-10-08 11:49:34 +0100

    Model

---
+++ src/moneycalculator/model/ExchangeRate.java
@@ -0,0 +1,43 @@
+package moneycalculator.model;
+
+import java.util.Date;
+
+public class ExchangeRate {
+
+    private Currency from;
+    private Currency to;
+    private Number rate;
+    private Date date;
+
+    public ExchangeRate(Date date, Currency from, Currency to, Number rate) {
+        this.date = date;
+        this.from = from;
+        this.to = to;
+        this.rate = rate;
+    }
+
+    public ExchangeRate(Currency from, Currency to, Number rate) {
+        this(new Date(), from, to, rate);
+    }
+
+    public Currency getFrom() {
+        return from;
+    }
+
+    public Currency getTo() {
+        return to;
+    }
+
+    public Number getRate() {
+        return rate;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    @Override
+    public String toString() {
+        return "ExchangeRate{" + "from=" + from + ", to=" + to + ", rate=" + rate + '}';
+    }
+}
