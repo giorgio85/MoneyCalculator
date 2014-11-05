1b4bb1d40532651d8ed1bb1d17b52a885745e3fa
usuario <usuario@10.22.143.27>
2014-11-05 12:10:24 +0000

    Funcionante 05/11/2014

--- src/persistence/CurrencySetLoader.java
+++ src/persistence/CurrencySetLoader.java
@@ -4,9 +4,13 @@ import model.Currency;
 import model.CurrencySet;
 
 public class CurrencySetLoader {
+    private final CurrencySet currencySet;
+    
+    public CurrencySetLoader(){
+        currencySet = new CurrencySet();
+    }
 
     public CurrencySet load() {
-        CurrencySet currencySet = new CurrencySet();
         currencySet.add(new Currency("EUR", "Euro", "€"));
         currencySet.add(new Currency("GBP", "Pounds", "£"));
         currencySet.add(new Currency("USD", "USD Dollar", "$"));
